5b87267585e64859405e82c39c13cf64b46533b4
Matheus <matheus.lima@ibbx.tech>
2023-08-24 16:47:29 -0300

    Sobre o memo

--- src/pages/Transactions/components/Searchform/index.tsx
+++ src/pages/Transactions/components/Searchform/index.tsx
@@ -5,6 +5,18 @@ import * as z from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { TransactionsContext } from "../../../../context/TransactionsContext";
 import { useContextSelector } from "use-context-selector";
+import { memo } from "react";
+
+// Porque um Componente Renderiza?
+
+/* 
+  Hooks changed
+  Props changed
+*/
+
+//O que pe o memo:
+//0.1: comparar a versao antiga dos hooks e props
+//0.2: Se mudou algo, ele vai permitir a nova Renderização
 
 const searchformScrema = z.object({
   query: z.string(),
